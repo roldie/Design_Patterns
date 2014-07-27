967c5fa52373c73a0a80a84070e07cdaccac9793
Harold Serrano <harold.serrano@gmail.com>
2014-07-27 12:14:37 -0700

    Initial Commit

---
+++ Design_Patterns/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Design_Patterns
+//
+//  Created by Harold Serrano on 7/27/14.
+//  Copyright (c) 2014 Roldie.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
