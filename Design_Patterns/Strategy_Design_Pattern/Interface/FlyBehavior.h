fb1f075cfe4d4715fba432f966352202b6788a7f
Harold Serrano <harold.serrano@gmail.com>
2014-08-01 21:06:17 -0700

    Strategy pattern initial files

---
+++ Design_Patterns/Strategy_Design_Pattern/Interface/FlyBehavior.h
@@ -0,0 +1,18 @@
+//
+//  FlyBehavior.h
+//  Design_Patterns
+//
+//  Created by Harold Serrano on 8/1/14.
+//  Copyright (c) 2014 Roldie.com. All rights reserved.
+//
+
+#ifndef Design_Patterns_FlyBehavior_h
+#define Design_Patterns_FlyBehavior_h
+
+class FlyBehavior{
+    
+public:
+    virtual fly(){}=0;
+};
+
+#endif
---
+++ Design_Patterns/Strategy_Design_Pattern/Interface/QuackBehavior.h
@@ -0,0 +1,18 @@
+//
+//  QuackBehavior.h
+//  Design_Patterns
+//
+//  Created by Harold Serrano on 8/1/14.
+//  Copyright (c) 2014 Roldie.com. All rights reserved.
+//
+
+#ifndef Design_Patterns_QuackBehavior_h
+#define Design_Patterns_QuackBehavior_h
+
+class QuackBehavior{
+    
+public:
+    virtual Quack(){}=0;
+};
+
+#endif
